e7b6f453f087383f064f15483a7e334413e7f463
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-29 20:14:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,8 @@ end
 def self.find_by_name(name)
   self.all.find {|x| x.name == name}
 end
-  
-  
+def self.find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name(name)
+end
   
 end
